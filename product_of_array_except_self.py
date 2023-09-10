d1e554c45056cdb3c0eb1139c711b52a12adcf6b
kavi-ta <kavisharma2807@gmail.com>
2023-09-10 16:25:29 +0530

    Solution to Product of Array Except Self - improvised

--- product_of_array_except_self.py
+++ product_of_array_except_self.py
@@ -4,23 +4,16 @@
 # 3. step1. update the result[pointer_from_left]= value_from_left and result[pointer_from_right]= value_from_right
 #  this way => update the current index with the mutlipliers from both sides of the current value without including the current value
 # 4. step2. update the value_from_left *= nums[pointer_from_left] and value_from_right *= nums[pointer_from_right] thereby picking up the current value at the respective indexes as a multiplier for the next result index
- 
+
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
             value_from_left = 1
             value_from_right = 1
+            n = len(nums)-1
             result = [1]*len(nums)
             for i in range(len(nums)):
-                pointer_from_left = i
-                pointer_from_right = len(nums)-1-i
-                # first update the current result index
-                if pointer_from_left == pointer_from_right:
-                    result[pointer_from_left] = value_from_left * value_from_right
-                else:
-                    result[pointer_from_left] *= value_from_left
-                    result[pointer_from_right] *= value_from_right
-
-                # update the values
-                value_from_left *= nums[pointer_from_left]
-                value_from_right *= nums[pointer_from_right]
+                result[i] *= value_from_left
+                result[n-i] *= value_from_right
+                value_from_left *= nums[i]
+                value_from_right *= nums[n-i]
             return result
