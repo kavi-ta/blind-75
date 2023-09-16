33a07ef0f071d58ded4dc1c50ad2118189576b3c
kavi-ta <kavisharma2807@gmail.com>
2023-09-16 16:11:33 +0530

    Solution to Container With Most Water

--- container_with_most_water.py
+++ container_with_most_water.py
@@ -3,14 +3,16 @@ class Solution:
         left_index = 0
         right_index = len(height)-1
         max_area = 0
+        left_height = height[left_index]
+        right_height = height[right_index]
         while(left_index<right_index):
-            left_height = height[left_index]
-            right_height = height[right_index]
+            
             max_area = max(max_area,abs(left_index-right_index) * min(left_height, right_height))
             if left_height>right_height:
                 right_index-=1
-            elif right_height>left_height:
+                right_height = height[right_index]
+            elif right_height>=left_height:
                 left_index+=1
-            else:
-                left_index+=1
+                left_height = height[left_index]
+
         return max_area
